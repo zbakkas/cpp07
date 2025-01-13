ec3538b105dd04f030e43763008dfd7b17cec759
Zouhir Bakkas <zouhirbakkas@gmail.com>
2025-01-13 22:18:30 +0100

    ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,12 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+#include <iostream>
+
+template <typename T , typename F>
+void iter(T* array, int length, F func) {
+    for (int i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
+
+#endif 
---
+++ ex01/main.cpp
@@ -0,0 +1,15 @@
+#include "iter.hpp"
+
+template <typename T>
+void print(T  &x) {
+    // return (int)x;
+    std::cout << x << std::endl;
+}
+
+int main()
+{
+    int tab[] = {0, 1, 2, 3, 4};
+    char tab2[] = {'a', 'b', 'c', 'd', 'e'};
+    ::iter(tab2, 5, ::print<char>);
+    return 0;
+}
